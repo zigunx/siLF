09c47edf6fa4be4465581911dbf31aa8015769ce
Hardi Agunadi <zigun_x@ymail.com>
2016-01-20 16:24:34 +0700

    working again

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -19,10 +19,33 @@
 					<div class="panel-heading">Home</div>
 
 					<div class="panel-body">
-						You are logged in!
+						Selamat Datang  {{ Auth::user()->name }}
+
+
 					</div>
+					</div>
+					<div class="panel-heading">
+					 <!-- DONUT CHART -->
+			          <div class="box box-danger">
+			            <div class="box-header with-border">
+			              <h3 class="box-title">Data Mahasiswa </h3>
+
+			              <div class="box-tools pull-right">
+			                <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+			                </button>
+			                <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
+			              </div>
+			            </div>
+			            <div class="box-body">
+			              <canvas id="pieChart" style="height:250px"></canvas>
+			            </div>
+			          </div> <!-- /.box-body -->
+					</div> <!-- /.heading -->
 				</div>
 			</div>
 		</div>
 	</div>
+	
+
 @endsection
+
---
+++ resources/views/layouts/mahasiswa/index.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts/app')
+
+@section('htmlheader_title')
+	Laman Mahasiswa
+@endsection
+@section('contentheader_title')
+	Mahasiswa
+@endsection
+@section('contentheader_description')
+	Daftar Mahasiswa
+@endsection
+
+
+@section('main-content')
+<div class="container spark-screen">
+		<div class="row">
+			<div class="col-md-10 col-md-offset-1">
+				<div class="panel panel-default">
+					<div class="panel-heading">Home</div>
+
+					<div class="panel-body">
+
+
+						
+
+					</div>
+					</div>
+				
+				</div>
+			</div>
+		</div>
+	</div>
+	
+@endsection
+
